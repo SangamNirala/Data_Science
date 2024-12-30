6221655444ed877506ad3448da6f8ccceb868675
SangamNirala <163988716+SangamNirala@users.noreply.github.com>
2024-12-30 06:32:32 +0530

    Add files via upload

---
+++ Artificial_Intelligence/CREW AI/Video_Blog_maker/agents.py
@@ -0,0 +1,48 @@
+from crewai import Agent
+from tools import yt_tool
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+import os
+# Option 1. Gemini accessed with an API key.
+# https://ai.google.dev/gemini-api/docs/api-key
+GEMINI_API_KEY="AIzaSyAajD3qOM9zyrQxKVKRbCMNp3RlvTzxTAg"
+
+# Option 2. Vertex AI IAM credentials for Gemini, Anthropic, and anything in the Model Garden.
+# https://cloud.google.com/vertex-ai/generative-ai/docs/overview
+
+
+## Create a senior blog content researcher
+
+blog_researcher=Agent(
+    role='Blog Researcher from Youtube Videos',
+    goal='get the relevant video transcription for the topic {topic} from the provided Yt channel',
+    verboe=True,
+    memory=True,
+    backstory=(
+       "Expert in understanding videos in AI Data Science , MAchine Learning And GEN AI and providing suggestion" 
+    ),
+    tools=[yt_tool],
+    allow_delegation=True
+)
+
+## creating a senior blog writer agent with YT tool
+
+blog_writer=Agent(
+    role='Blog Writer',
+    goal='Narrate compelling tech stories about the video {topic} from YT video',
+    verbose=True,
+    memory=True,
+    backstory=(
+        "With a flair for simplifying complex topics, you craft"
+        "engaging narratives that captivate and educate, bringing new"
+        "discoveries to light in an accessible manner."
+    ),
+    tools=[yt_tool],
+    allow_delegation=False
+
+
+)
